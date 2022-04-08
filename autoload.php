2aec037887065fc26bba1413ee5b069f52692de0
Your Name <ostap.lyashenko25gmail.com>
2022-04-08 17:43:03 +0300

    rgb

--- autoload.php
+++ autoload.php
@@ -3,19 +3,15 @@
 class Autoloader
 {
     protected $prefixes = array();
-    public function addNamespace($prefix, $base_dir, $prepend = false)
-    {
-        $prefix = trim($prefix, '\\') . '\\';
 
-        $base_dir = rtrim($base_dir, DIRECTORY_SEPARATOR) . '/';
+    public function addNamespace(string $prefix, string $dir)
+    {
+        $prefix .= '\\';
+        $dir .= '/';
         if (isset($this->prefixes[$prefix]) === false) {
             $this->prefixes[$prefix] = array();
         }
-        if ($prepend) {
-            array_unshift($this->prefixes[$prefix], $base_dir);
-        } else {
-            $this->prefixes[$prefix][] = $base_dir;
-        }
+        array_push($this->prefixes[$prefix], $dir);
     }
 
     public function register()
@@ -25,32 +21,24 @@ class Autoloader
 
     public function autoload($class)
     {
+        $prefix = $class;
         while (false !== $pos = strrpos($prefix, '\\')) {
+            $pos = strrpos($prefix, '\\');
             $prefix = substr($class, 0, $pos + 1);
-            $class = substr($class, $pos + 1);
-            $mapped_file = $this->autoload($class);
-            if ($mapped_file) {
-                return $mapped_file;
+            $relative_class = substr($class, $pos + 1);
+            if (isset($this->prefixes[$prefix]) !== false) {
+                foreach ($this->prefixes[$prefix] as $dir) {
+                    $file = $dir . str_replace('\\', '/', $relative_class) . '.php';
+                    require_once $file;
+                }
+                echo "<pre>";
+                var_dump($file);
+                echo  "</pre>";
             }
-            $prefix = rtrim($prefix, '\\')
-            .str_replace('\\', '/', $class)
-            . '.php';
-
-            if ($this->requireFile($class)) {
-                return $class;
-            }
+            $prefix = rtrim($prefix, '\\');
         }
-    }
-    protected function requireFile($class)
-    {
-        if (file_exists($class)) {
-            require $class;
-            return true;
-        }
-        return false;
     }
 }
 $autoloader = new Autoloader();
 $autoloader->addNamespace('Hillel', 'src');
 $autoloader->register();
-var_dump($autoloader);
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 declare(strict_types=1);
 
-$dbh = new PDO('mysql:host=mysql;port=3306;dbname=sandbox', 'root', 'secret');
-
-require_once '../autoload.php';
+require_once 'autoload.php';
 
 $user = new \Hillel\Models\Users();
 $c1 = new \Hillel\ValueObjects\RgbColor();
 $action = new \Hillel\Controlers\http\HomeAction();
+
+var_dump($user);
+var_dump($action);
+var_dump($c1);
